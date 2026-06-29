f3f2a0ac933eac684043f89223cb2579f64e8927
keerthi <keerthigondadgiokeerthigondadg.com>
2026-06-29 16:38:37 +0530

    fastapinotes

---
+++ frontend/talentspark/src/App.tsx
@@ -0,0 +1,20 @@
+
+import Welcome from "./components/Welcome";
+import NavBar from "./components/NavBar";
+import CompanyCard  from "./components/CompanyCard";
+import JobCard from "./components/JobCard";
+import Footer from "./components/Footer";
+function App (){
+  return(
+    <>
+    <Welcome/>
+    <NavBar/>
+    <CompanyCard />
+    <JobCard />
+    <Footer />
+    
+    </>
+  )
+}
+
+export default App
