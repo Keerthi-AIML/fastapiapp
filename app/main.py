16fa0338b7ffe245d8b93da3e28b96b667838c6a
keerthi <keerthigondadgiokeerthigondadg.com>
2026-07-08 14:36:21 +0530

    fastapinotes

--- app/main.py
+++ app/main.py
@@ -1,3 +1,49 @@
-from backend.app.main import app
+from fastapi import FastAPI
+from routers import company,job,auth,chat,rag
+from database import Base,engine
+from models import job as job_model,company as company_model,users as user_model
+from fastapi.middleware.cors import CORSMiddleware
 
-__all__ = ["app"]
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+# Base.metadata.create_all(bind=engine)
+@app.on_event("startup")
+async def startup_event():
+    from database import engine
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+app.include_router(auth.router)
+app.include_router(company.router)
+app.include_router(job.router)
+app.include_router(chat.router)
+app.include_router(rag.router)
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/about")
+def read_about():
+    return {"about": "This is about page"}
+
+@app.get("/contact")
+def read_contact():
+    return {"contact": "This is contact page"}
+#  Albattrosdip
+# steps---> 
+#  1.postgres drivers
+# 2.servers
+# 3. registration -> enterprise db->first two options
+# student_db>database>schemas>tables>right click>query tool
